4e6756e73cf444c0386de92b9698dba95e9327c2
matheus abrantes <abranteselcio@gmail.com>
2024-08-06 20:43:51 -0300

    feat: add controls scene

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,9 +2,11 @@ extends Control
 
 @onready var menu: Control = get_node("menu")
 @onready var button_container: VBoxContainer = menu.get_node("menu/buttonContainer")
+@onready var bgroundSprite: AnimatedSprite2D = get_node("bground")
 #@onready var continue_button: Button = button_container.get_node("continue")
 
 func _ready():
+	bgroundSprite.play("menu-anim")
 	#var file: File = File.new()
 	pass
 
@@ -14,3 +16,7 @@ func _on_play_pressed():
 
 func _on_sair_pressed():
 	get_tree().quit()
+
+
+func _on_opções_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/controls.tscn")
