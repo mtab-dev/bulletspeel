432ad9cf19c364e7dcb66b0656833b2769fb8fe1
mtab-dev <abranteselcio@gmail.com>
2024-08-19 16:03:19 -0300

    fx: player sound effect when walking

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,13 +5,21 @@ extends CharacterBody2D
 @onready var money: Control = get_node("hud/money/labelMoney")
 @onready var stats: Control = get_node("hud/life/control")
 @onready var damageFX: AudioStreamPlayer2D = get_node("damageSound")
+@onready var walkingFX: AudioStreamPlayer2D = $walkingSound
 @onready var deathTimer: Timer = $deathTimer
 @onready var lolli: Sprite2D = get_node('hud/items/loli')
 var moneyCount: int = 0
 var hasLolli: bool = false
 var isDead: bool = false
+var isWalking: bool = false
 var anim: String = 'idle'
 
+func walkingEffect():
+	isWalking = velocity.x != 0 or velocity.y != 0
+	if isWalking and not walkingFX.playing:
+		walkingFX.play()
+	elif !isWalking and walkingFX.playing:
+		walkingFX.stop()
 
 func timeAfterDeath():
 	deathTimer.wait_time = 2.0
@@ -21,11 +29,15 @@ func timeAfterDeath():
 func horizontalMovement() -> void:
 	var input_direction: float = Input.get_action_raw_strength("right") - Input.get_action_strength("left")
 	velocity.x = input_direction * speed
+	if velocity.x != 0:
+		isWalking = true
 	print(velocity.x)
 	
 func verticalMovement() -> void:
 	var input_direction: float = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity.y = input_direction * speed
+	if velocity.y != 0:
+		isWalking = true
 	print(velocity.y)
 	
 func movement():
@@ -45,8 +57,8 @@ func updateItems():
 
 func lifeManagement():
 	if(Input.is_action_just_pressed("space")):
+		damageFX.play()	
 		Global.life -= 1
-		damageFX.play()
 		if(Global.life <= 0):
 			get_tree().paused = true
 			isDead = true
@@ -65,7 +77,7 @@ func _physics_process(delta):
 	animation.animate(velocity, Global.life)
 	movement()
 	move_and_slide()
-
+	walkingEffect()
 
 func _on_death_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/ui/game_over.tscn")
