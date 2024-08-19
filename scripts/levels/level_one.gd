1b815af900979912b8cb0764124d61289958a0c7
mtab-dev <abranteselcio@gmail.com>
2024-08-19 14:44:51 -0300

    feat: add chest sprites and events when bullet collide to it

---
+++ scripts/levels/level_one.gd
@@ -0,0 +1,33 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_chest_1_body_entered(body):
+	if body.is_in_group('Player'):
+		var chest = get_node('lvlDesign/collision/chest1/chest')
+		if chest:
+			chest.queue_free()
+
+
+func _on_chest_2_body_entered(body):
+	if body.is_in_group('Player'):
+		var chest = get_node('lvlDesign/collision/chest2/chest2')
+		if chest:
+			chest.queue_free()
+
+
+
+func _on_chest_3_body_entered(body):
+	if body.is_in_group('Player'):
+		var chest = get_node('lvlDesign/collision/chest3/chest3')
+		if chest:
+			chest.queue_free()
