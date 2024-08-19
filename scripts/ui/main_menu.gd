49f1baf7373d667d8ba528848cf4db7fc74c7822
mtab-dev <abranteselcio@gmail.com>
2024-08-19 16:37:18 -0300

    fx: mainMenu sounds and button effects

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,19 +2,41 @@ extends Control
 
 @onready var menu: Control = get_node("menu")
 @onready var bgroundSprite: AnimatedSprite2D = get_node("bground")
-#@onready var continue_button: Button = button_container.get_node("continue")
+@onready var clickSound: AudioStreamPlayer2D = $buttonClick
+@onready var sceneMorph: AudioStreamPlayer2D = $sceneMorph
+@onready var changeTimer: Timer = $sceneChangingTimer
+var path: String
 
 func _ready():
 	bgroundSprite.play("menu-anim")
-	pass
+
+
+func timerAwait(time: float):
+	changeTimer.wait_time = time
+	changeTimer.one_shot = true
+	changeTimer.start()
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/ui/introduction.tscn")
+	path = 'play'
+	sceneMorph.play()
+	timerAwait(0.6)
 
 
 func _on_sair_pressed():
-	get_tree().quit()
-
+	path = 'quit'
+	clickSound.play()
+	timerAwait(0.9)
 
 func _on_opções_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/controls.tscn")
+	path = 'controls'
+	clickSound.play()
+	timerAwait(0.9)
+
+
+func _on_scene_changing_timer_timeout():
+	if path == 'play':
+		get_tree().change_scene_to_file("res://scenes/ui/introduction.tscn")	
+	elif path == 'controls':
+		get_tree().change_scene_to_file("res://scenes/levels/controls.tscn")
+	elif path == 'quit':
+		get_tree().quit()
