862a4f06584f25aceb5fa016c862e2093d763f4f
mtab-dev <abranteselcio@gmail.com>
2024-08-21 16:25:26 -0300

    feat: bengal enemy life system

--- scripts/enemies/bengal/bengal.gd
+++ scripts/enemies/bengal/bengal.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animation: AnimatedSprite2D = $bengalAnimation
 @onready var damageSound: AudioStreamPlayer = $damage
+var bengalLife: int = 4
 
 func _ready():
 	pass
@@ -19,3 +20,7 @@ func _on_detection_area_body_entered(body):
 	if(body.is_in_group('Player')):
 		damageSound.play()
 		Global.life -=1
+	if(body.is_in_group('Bullet')):
+		bengalLife -= 1
+		if bengalLife <= 0:
+			queue_free()
