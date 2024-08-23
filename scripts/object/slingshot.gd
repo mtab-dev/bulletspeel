af32d342fb1bb000c97f54c3c761a526b866e4c7
gustavo <gustavomatias060106@gmail.com>
2024-08-23 18:16:45 -0300

    feat: add mouse rotation by the slingshot

--- scripts/object/slingshot.gd
+++ scripts/object/slingshot.gd
@@ -17,3 +17,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("left_click"):
 		print('clickou')
 		shoot()
+		
+func _process(delta: float) -> void:
+	rotate(get_angle_to(get_global_mouse_position()))
