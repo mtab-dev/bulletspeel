bc2185737b4c255e82bded61289885222f0f7b27
mtab-dev <abranteselcio@gmail.com>
2024-09-11 17:36:39 -0300

    fix: click to change scene at introduction

--- scripts/ui/introduction.gd
+++ scripts/ui/introduction.gd
@@ -11,5 +11,13 @@ func _process(delta):
 	pass
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch and event.pressed:
+		get_tree().change_scene_to_file("res://scenes/levels/level_one.tscn")
+	if event is InputEventKey and event.pressed:
+		get_tree().change_scene_to_file("res://scenes/levels/level_one.tscn")
+
+
 func _on_go_ahead_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/level_one.tscn")	
+	get_tree().change_scene_to_file("res://scenes/levels/level_one.tscn")
+	
