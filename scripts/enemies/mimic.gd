ffa1d63a68f9f336c8c140cac7b97610b1ec3932
mtab-dev <abranteselcio@gmail.com>
2024-09-25 16:42:32 -0300

    refactor: remove trash code

--- scripts/enemies/mimic.gd
+++ scripts/enemies/mimic.gd
@@ -8,28 +8,15 @@ var player: CharacterBody2D
 
 func _ready():
 	player = get_tree().get_first_node_in_group("Player")
-	#animation.play('goingUp')
 	health = 6
 	healthBar.initHealth(health)
 	
-func repeatAnim():
-	#if isInHit == true:
-		#Global.life -= 1
-		#animation.play('attack')
-	pass
-
 func _physics_process(delta):
 	if velocity.x > 0:
 		animation.flip_h = false
 	else:
 		animation.flip_h = true
 	move_and_slide()
-
-func _on_hit_box_body_entered(body: Node2D) -> void:
-	#if body.is_in_group('Player'):
-		#isInHit = true
-		#animation.play('attack')
-	pass
 
 
 func _on_hit_box_area_entered(area: Area2D) -> void:
@@ -38,14 +25,3 @@ func _on_hit_box_area_entered(area: Area2D) -> void:
 		healthBar.health = health  
 		if health <= 0:
 			queue_free()
-
-
-func _on_animation_animation_finished() -> void:
-	#if isInHit:
-		#repeatAnim()
-	pass
-
-func _on_hit_box_body_exited(body: Node2D) -> void:
-	#if body.is_in_group('Player'):
-		#isInHit = false
-	pass
