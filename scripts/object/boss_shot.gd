74040d5bab405bd83321e0d7aa484f12b394b0b9
mtab-dev <abranteselcio@gmail.com>
2024-11-26 17:56:47 -0300

    refactor: remove damage from the boss bullets, waiting for correction at player damage logic

--- scripts/object/boss_shot.gd
+++ scripts/object/boss_shot.gd
@@ -5,16 +5,12 @@ const speed = 200
 func _process(delta: float) -> void:
 	position += transform.x * speed * delta
 
-
 func _on_kill_timer_timeout() -> void:
 	queue_free()
 
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		queue_free()
-		Global.life -= 1
-	else: queue_free()
+	queue_free()
 
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
