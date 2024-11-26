88260a65dbde1d12dc8744cda671c469a8652abd
mtab-dev <abranteselcio@gmail.com>
2024-11-26 15:21:44 -0300

    refactor: remove unused loc

--- scripts/ui/hudControl.gd
+++ scripts/ui/hudControl.gd
@@ -13,7 +13,6 @@ func _ready():
 		joystick.visible = true
 	else:
 		joystick.visible = false
-	print(OS.get_name())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
