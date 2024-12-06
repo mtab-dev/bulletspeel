7802cee9e494ee09dffe8b7555760680f68e15e9
matheus abrantes <abranteselcio@gmail.com>
2024-12-06 18:41:51 -0300

    fix: reset stats when restart by level menu

--- scripts/ui/gameOver.gd
+++ scripts/ui/gameOver.gd
@@ -8,7 +8,6 @@ func restartGame():
 	Global.hasBoots = false
 	Global.hasLolli = false
 	Global.death += 1
-	print(Global.death)
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/levels/level_one.tscn")
 
