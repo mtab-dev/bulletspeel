b1f6af576a6f7c8e8b59ff6bf35483756ded6968
matheus abrantes <abranteselcio@gmail.com>
2024-12-14 12:58:38 -0300

    fix: boss bullets damage player

--- scripts/object/boss_shot.gd
+++ scripts/object/boss_shot.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const speed = 200
+const speed = 150
 
 func _process(delta: float) -> void:
 	position += transform.x * speed * delta
@@ -10,6 +10,8 @@ func _on_kill_timer_timeout() -> void:
 
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("Player")):
+		Global.life -=1;
 	queue_free()
 
 
